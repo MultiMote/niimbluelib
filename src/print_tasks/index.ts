a23f1a0076c4574cc63db692ad7d4339feea5db0
MultiMote <contact@mmote.ru>
2024-10-21 14:04:52 +0300

    Rename D11_OLD to D11_V1

--- src/print_tasks/index.ts
+++ src/print_tasks/index.ts
@@ -6,7 +6,7 @@ import { OldD11PrintTask } from "./OldD11PrintTask";
 import { V5PrintTask } from "./V5PrintTask";
 
 export const printTasks = {
-  D11_OLD: OldD11PrintTask,
+  D11_V1: OldD11PrintTask,
   D110: D110PrintTask,
   B1: B1PrintTask,
   B21_V1: B21V1PrintTask,
@@ -18,7 +18,7 @@ export type PrintTaskName = keyof typeof printTasks;
 export const printTaskNames = Object.keys(printTasks) as PrintTaskName[];
 
 export const modelPrintTasks: Partial<Record<PrintTaskName, M[]>> = {
-  D11_OLD: [M.D11, M.D11S],
+  D11_V1: [M.D11, M.D11S],
   B21_V1: [M.B21, M.B21_L2B, M.B21_C2B],
   D110: [M.B21S, M.B21S_C2B, M.D110],
   B1: [M.D11_H, M.D110_M, M.B1],
